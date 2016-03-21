5e47829a9ddd072ceaf15cbaa59f4c6ffefe94fe
cherishla <cherishla.fang@gmail.com>
2016-03-21 19:49:02 +0800

    0321HomeWork

---
+++ MVC5Homework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Homework.Startup))]
+namespace MVC5Homework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
